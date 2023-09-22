270f9b7464d72a515fef39cdfe26776ae08e05c1
Pablo Cabeza <josepablocg@gmail.com>
2023-09-22 19:45:57 +0100

    Some formatting

--- function_app.py
+++ function_app.py
@@ -14,7 +14,7 @@ from jobsearch.linkedin import LinkedInProcessing, load_cookies
 from jobsearch.models import JobInfo, LinkedinSearchLoad
 from jobsearch.openai import job_summary_openai
 from jobsearch.persistance import StorageLayer
-from function.job_search_entity import JobSearchWatermark 
+from function.job_search_entity import JobSearchWatermark
 
 app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -22,7 +22,9 @@ app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 # An HTTP-Triggered Function with a Durable Functions Client binding
 @app.route(route="orchestrators")
 @app.durable_client_input(client_name="client")
-async def http_start_job_search(req: func.HttpRequest, client: df.DurableOrchestrationClient):
+async def http_start_job_search(
+    req: func.HttpRequest, client: df.DurableOrchestrationClient
+):
     # function_name = req.route_params.get('functionName')
     keywords = "Senior Data Scientist"
     instance_id = await client.start_new("job_search_orchestrator", None, keywords)
@@ -30,7 +32,9 @@ async def http_start_job_search(req: func.HttpRequest, client: df.DurableOrchest
     keywords = "Senior Data Engineer"
     instance_id = await client.start_new("job_search_orchestrator", None, keywords)
 
-    response = await client.wait_for_completion_or_create_check_status_response(req, instance_id)
+    response = await client.wait_for_completion_or_create_check_status_response(
+        req, instance_id
+    )
     return response
 
 
@@ -46,7 +50,7 @@ class SearchParam:
 def job_search_orchestrator(context: df.DurableOrchestrationContext):
     keywords = context.get_input()
     logging.warn(keywords)
-    
+
     current_time: JobSearchWatermark = yield context.call_activity("current_time")
 
     entityId = df.EntityId("JobSearchWatermark", keywords)
@@ -56,13 +60,18 @@ def job_search_orchestrator(context: df.DurableOrchestrationContext):
     diff = (current_time.value - prev_time.value).seconds
 
     # Search for jobs
-    raw_search_results: LinkedinSearchLoad = yield context.call_activity("search", SearchParam(keywords, diff))
+    raw_search_results: LinkedinSearchLoad = yield context.call_activity(
+        "search", SearchParam(keywords, diff)
+    )
 
     # Store in cosmos
     yield context.call_activity("persists_query_in_cosmos", raw_search_results)
 
     # Run processing for each job result
-    tasks = [context.call_sub_orchestrator("job_info_orchestrator", result) for result in raw_search_results.jobs_reply]
+    tasks = [
+        context.call_sub_orchestrator("job_info_orchestrator", result)
+        for result in raw_search_results.jobs_reply
+    ]
     job_infos: Sequence[JobInfo] = yield context.task_all(tasks)
 
     # Update timestamp
@@ -88,7 +97,7 @@ def job_info_orchestrator(context: df.DurableOrchestrationContext):
 
     # Set it as run
     yield context.call_entity(entity, "set")
-    
+
     return job_info
 
 
@@ -106,37 +115,51 @@ def search(query: SearchParam):
 
     load_time = datetime.now()
     listed_offset_seconds = query.listed_offset
-    jobs_reply = api.search_remote_jobs(query.keywords, listed_offset_seconds=listed_offset_seconds)
+    jobs_reply = api.search_remote_jobs(
+        query.keywords, listed_offset_seconds=listed_offset_seconds
+    )
 
     search_load = LinkedinSearchLoad(
         loadtime=load_time,
         keywords=query.keywords,
         listed_offset_seconds=listed_offset_seconds,
-        jobs_reply=jobs_reply
+        jobs_reply=jobs_reply,
     )
     logging.info(f"query retuned {len(jobs_reply)} results")
 
     storage = StorageLayer()
-    storage.persist_on_storage("raw/job_searches/" + search_load.get_path_part(), search_load.get_file_name(), search_load)
+    storage.persist_on_storage(
+        "raw/job_searches/" + search_load.get_path_part(),
+        search_load.get_file_name(),
+        search_load,
+    )
 
     return search_load
 
+
 @app.activity_trigger(input_name="input")
-@app.cosmos_db_output(arg_name="cosmos", database_name="linkedinjobs", container_name="query", partition_key="/query", create_if_not_exists=True, connection="CosmosConnection")
-def persists_query_in_cosmos(input: LinkedinSearchLoad, cosmos: func.Out[func.DocumentList]):
+@app.cosmos_db_output(
+    arg_name="cosmos",
+    database_name="linkedinjobs",
+    container_name="query",
+    partition_key="/query",
+    create_if_not_exists=True,
+    connection="CosmosConnection",
+)
+def persists_query_in_cosmos(
+    input: LinkedinSearchLoad, cosmos: func.Out[func.DocumentList]
+):
     query = input.keywords
     raw_jobs = input.jobs_reply
 
     messages = func.DocumentList()
     for job in raw_jobs:
         job_id = LinkedInProcessing.get_id_from_job(job)
-        message = {
-            "id": job_id,
-            "query": query
-        }
+        message = {"id": job_id, "query": query}
         messages.append(func.Document.from_dict(message))
     cosmos.set(messages)
     return ""
+
 
 @app.activity_trigger(input_name="job")
 def query_info(job: dict) -> JobInfo:
@@ -147,14 +170,19 @@ def query_info(job: dict) -> JobInfo:
     job_info = api.get_job_info(job)
     try:
         job_info.summary = job_summary_openai(job_info.description).to_dataclass()
-    except Exception as e: 
+    except Exception as e:
         logging.exception(f"Error while processing {job_info}")
 
     return job_info.to_dict()
 
 
 @app.activity_trigger(input_name="job")
-@app.cosmos_db_output(arg_name="cosmos", database_name="linkedinjobs", container_name="jobs", connection="CosmosConnection")
+@app.cosmos_db_output(
+    arg_name="cosmos",
+    database_name="linkedinjobs",
+    container_name="jobs",
+    connection="CosmosConnection",
+)
 def persist_job_info(job, cosmos: func.Out[func.Document]):
     data = job.to_dict()
     data["id"] = data["job_posting_id"]
@@ -163,6 +191,7 @@ def persist_job_info(job, cosmos: func.Out[func.Document]):
 
 
 from function import job_search_entity, job_process_entity
+
 app.register_functions(job_search_entity.app)
 app.register_functions(job_process_entity.app)
 
